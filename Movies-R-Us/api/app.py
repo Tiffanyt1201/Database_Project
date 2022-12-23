e3a8ff1b7f51f91ca8f6d752847fdbcfd033330c
Tiffanyt1201 <46972701+Tiffanyt1201@users.noreply.github.com>
2022-12-22 22:09:08 -0700

    Added CPSC 471 Project

--- Movies-R-Us/api/app.py
+++ Movies-R-Us/api/app.py
@@ -14,7 +14,8 @@ app = Flask(__name__)
 '''
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Bubbledophins1201!'
+// Enter own MySQL Password
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'cpsc471_project_db'
 '''
 * References: 
